700d63875a3a76e84d5b006bd2d20a6e2a3afcc3
Skanda Kaashyap <skandakk@gmail.com>
2021-09-01 22:32:10 -0500

    included config parser and updated selfie_img_handler endpoint

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for,session,request, render_template
+from flask import Flask, redirect, url_for,session,request, render_template, abort
 import responses
 import os, sys
 from azure.core.exceptions import ResourceNotFoundError
@@ -6,15 +6,33 @@ from azure.ai.formrecognizer import FormRecognizerClient
 from azure.ai.formrecognizer import FormTrainingClient
 from azure.core.credentials import AzureKeyCredential
 from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient, __version__
+from msrest.authentication import CognitiveServicesCredentials
+
+from azure.cognitiveservices.vision.face import FaceClient
 from shutil import copyfile
 from datetime import date
 import base64
 from io import BytesIO
 from PIL import Image
+import pyodbc
+
+import configparser
+
 
 # Define a flask app
 app = Flask(__name__)
-app.secret_key='trafficaid'
+
+class Data: 
+    def __init__(self):
+        self.id_image_path = None
+        self.selfie_image = None
+        self.not_underage = False
+        self.username = ""
+        self.image_not_uploaded = True
+
+data = Data()
+
+CONFIDENCE_THRESHOLD = 0.5
 
 
 # Route the user to the homepage
@@ -41,7 +59,7 @@ def id_img_handler():
     with open(save_loc, "wb"):
         image.save(save_loc)
 
-    store_custid_image_and_path("wmt_user", save_loc)
+    store_custid_image_and_path("wmt_user8", save_loc)
 
     id_api_endpoint = "https://formrecognizeid.cognitiveservices.azure.com/"
 
@@ -55,10 +73,10 @@ def id_img_handler():
         data.not_underage = True
         data.image_not_uploaded = False
 
-        return responses.get_http_200(message="true")
+        return responses.get_http_200()
     else:
         print("User is NOT older than 21... :(")
-        return responses.get_http_200(message="false")
+        return responses.get_http_200()
 
 
 @app.route("/api/upload_selfie", methods=["POST"])
@@ -75,15 +93,39 @@ def selfie_img_handler():
         im.save('./temp_uploads/selfie.png', 'PNG')
     # Load image from local into stream ->(selfie.png)
 
-    # target_image_file_name = open('./temp_uploads/image.png', 'r+b')
-    # target_faces2= face_client.face.detect_with_stream(target_image_file_name, detection_model='detection_03')
     # target_image_id = target_faces2[0].face_id
     # data.selfie_image = target_image_id
-    
+
     # TODO: write 
     # compare this image with the ID image
+    # use blob URL to obtain ID image
+    # compare faceids
+    # verify faces using direct url 
 
-    return render_template('index.html')
+    IMAGE_BASE_URL = "https://wmtcustinfo.blob.core.windows.net/wmtcustinfo/"
+    ENDPOINT = "https://face-wmt-hackathon.cognitiveservices.azure.com/"
+    face_client = FaceClient(ENDPOINT, CognitiveServicesCredentials(app.FACE_KEY))
+
+
+    source_image_file_name1 = IMAGE_BASE_URL + "wmt_user8" + "_user_identification_card.jpg" 
+
+    print("source_iage_file_name1:", source_image_file_name1)
+    source_faces1 = face_client.face.detect_with_url(source_image_file_name1, detection_model='detection_03')
+    source_image1_id = source_faces1[0].face_id
+
+    target_image_file_name = open('./temp_uploads/selfie.png', 'r+b')
+    target_faces2 = face_client.face.detect_with_stream(target_image_file_name, detection_model='detection_03')
+    target_face_id = target_faces2[0].face_id
+
+    result = face_client.face.verify_face_to_face(source_image1_id, target_face_id)
+    print("result:", result)
+
+    confidence = result.is_identical
+    if confidence >= CONFIDENCE_THRESHOLD:
+        return responses.get_http_200()
+
+    print("The images do not match.")
+    abort(404)
     
 
     
@@ -114,7 +156,7 @@ def extract_dob(endpoint,id_proof):
                        in order for this to work, we need to ensure that the image is saved locally
                        first 
     """
-    form_recognizer_client = FormRecognizerClient(endpoint=endpoint, credential=AzureKeyCredential(app.azure_secret))
+    form_recognizer_client = FormRecognizerClient(endpoint=endpoint, credential=AzureKeyCredential(app.OCR_KEY))
     with open(id_proof, "rb") as f:
         poller = form_recognizer_client.begin_recognize_identity_documents(identity_document=f)
     id_documents = poller.result()
@@ -129,22 +171,6 @@ def extract_dob(endpoint,id_proof):
         return age >= 21
     else:
         print('Issue with dob parsing')
-
-
-class Data: 
-    def __init__(self):
-        self.id_image_path = None
-        self.selfie_image = None
-        self.not_underage = False
-        self.username = ""
-        self.image_not_uploaded = True
-
-def get_secret_key():
-    with open("keys.txt") as f:
-        key = f.read()
-
-    print(key[key.find("=")+1:])
-    return key[key.find("=")+1:]
 
 
 def store_custid_image_and_path(username: str, save_loc:str):
@@ -162,7 +188,7 @@ def store_custid_image_and_path(username: str, save_loc:str):
     blob_service_client = BlobServiceClient.from_connection_string(connect_str)
     base_dir = os.path.dirname(save_loc)
     user_id_filename = F'{username}_user_identification_card.jpg'
-    upload_file_path = base_dir + user_id_filename
+    upload_file_path = base_dir + "/" + user_id_filename
     print("save_loc = {0}".format(save_loc))
     print("upload_file_path = {0}".format(upload_file_path))
     print("copying customer id to new file path")
@@ -183,11 +209,12 @@ def store_custid_image_and_path(username: str, save_loc:str):
         exit(1)
 
 if __name__ == '__main__':
-    app.secret_key="casdfnjakwhejfwefjkwnemwh87h"
-    app.azure_secret = get_secret_key()
+    config = configparser.ConfigParser()
+    config.read("properties.ini")
+    app.OCR_KEY = config["KEYS"]["ocr_key"]
+    app.FACE_KEY = config["KEYS"]["face_key"]
     app.config["UPLOAD_FOLDER"] = "temp_uploads/"
     app.run(debug=True, port=10061)
-    data = Data()
 
 
 
